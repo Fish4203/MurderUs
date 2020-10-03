b5f9dd9c5b040a48965644ef0a4d5fd6c5792623
Fish4203 <26777965+Fish4203@users.noreply.github.com>
2020-10-03 15:00:34 +1000

    half broken bit i kinda know what im doing mayby

--- mysite/chat/consumers.py
+++ mysite/chat/consumers.py
@@ -1,35 +1,51 @@
 import json
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.db import database_sync_to_async
+from channels.generic.websocket import AsyncWebsocketConsumer
+from django.contrib.auth.models import User
+from .models import Message
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
         # Join room group
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
 
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
+        await self.send(text_data=json.dumps({
+            'message': 'help me pls',
+            'user': self.user
+        }))
+
+    def get_name(self):
+        out = []
+        for user in User.objects.all():
+            out.append(user.username)
+        return out
+
+
+
+    async def disconnect(self, close_code):
         # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
     # Receive message from WebSocket
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
         user = text_data_json['user']
 
         # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
@@ -39,12 +55,12 @@ class ChatConsumer(WebsocketConsumer):
         )
 
     # Receive message from room group
-    def chat_message(self, event):
+    async def chat_message(self, event):
         message = event['message']
         user = event['user']
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'message': message,
             'user': user
         }))
--- mysite/chat/models.py
+++ mysite/chat/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Task(models.Model):
+    doneness = models.IntegerField()
+    type = models.CharField(max_length=200)
+    code = models.IntegerField()
+    var1 = models.CharField(max_length=200)
+    var2 = models.CharField(max_length=200)
+    var3 = models.CharField(max_length=200)
+
+class Player(models.Model):
+    name = models.CharField(max_length=200)
+    aliveness = models.IntegerField()
+    tag = models.IntegerField()
+    tasks = models.ManyToManyField(Task)
+    role = models.CharField(max_length=200)
+
+class Game(models.Model):
+    gameId = models.IntegerField()
+    meating = models.IntegerField()
+    tasks = models.ManyToManyField(Task)
+    players = models.ManyToManyField(Player)
+
+    def __str__(self):
+        return self.id
